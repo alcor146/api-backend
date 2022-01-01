9ed0cc8276ff70866841e90ff7b51e8c58f1f5f9
alcor146 <aurel_rist@yahoo.com>
2022-01-01 06:45:20 -0800

    db

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const Order = require('../models/Order')
+// const Order = require('../models/Order')
+const DATA = require('./databaseRecords')
+const Product = require('../models/Product')
 
 
 const connectDB = async () => {
@@ -14,12 +16,38 @@ const connectDB = async () => {
   })
 }
 
-var newOrder = new Order({
-  products: ["new", "item"],
-  status: "in progress"
-})
-newOrder.save();
+// var newOrder = new Order({
+//   products: ["new", "item"],
+//   status: "in progress"
+// })
+// newOrder.save();
+
+for(let i=0; i<DATA.length; i++) {
+  var newProduct = new Product({
+    name: DATA[i].name,
+    OS: DATA[i].OS,
+    internalMemory: DATA[i].internalMemory,
+    RAM: DATA[i].RAM,
+    processor: DATA[i].processor,
+    SIM: DATA[i].SIM,
+    SIMSlots: DATA[i].SIMSlots,
+    display: DATA[i].display,
+    displayResolution: DATA[i].displayResolution,
+    diplayDimensions: DATA[i].diplayDimensions,
+    dimensions: DATA[i].dimensions,
+    mainCamera: DATA[i].mainCamera,
+    frontalCamera: DATA[i].frontalCamera,
+    battery: DATA[i].battery,
+    price: DATA[i].price,
+    inStock: DATA[i].inStock
+  })
+
+  newProduct.save()
+}
+
+
+
+
 
 module.exports = connectDB;
 
-//mongodb-service.mongo.svc.cluster.local:27017
