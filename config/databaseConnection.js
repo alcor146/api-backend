0d869b184273ed4590032d6aaba2abf0a849ca52
Aurel <aristoiu@siqsess.ro>
2022-07-09 11:02:35 +0300

    qwef

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -96,11 +96,18 @@ async function populate() {
 }
 
 
+ //var mongoString = "mongodb://username:password@mongo-mongodb.mongo.svc:27017/licenta"
+ var mongoString = "mongodb://username:password@localhost:27017/licenta"
+
+
+
+
+
 
 
 const connectDB = async () => {
-  var mongoString = "mongodb://username:password@mongo-mongodb.mongo.svc:27017/licenta"
-  //var mongoString = "mongodb://username:password@localhost:27017/licenta"
+  
+  var mongoString = "mongodb://username:password@localhost:27017/licenta"
   console.log('connection string:', mongoString);
   await mongoose.connect(mongoString, {
     useNewUrlParser: true,
